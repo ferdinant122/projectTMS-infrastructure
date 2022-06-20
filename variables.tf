41d30f62454763b5f4afe865923379bdc27d9e4b
Dmitry Korzhel <91656391+ferdinant122@users.noreply.github.com>
2022-06-20 13:48:35 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,5 +2,5 @@ variable "region" { default = "eu-central-1" }
 variable "environment" { default = "" }
 variable "project" { default = "javaweb" }
 variable "platform" { default = "Devops" }
-variable "vpc_id" { default = "vpc-065b0cd4850ba32fb" }
+variable "vpc_id" { default = "" }
 variable "team" { default = "dkproject" }
