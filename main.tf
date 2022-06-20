d382c6aa75d8e389340c7b89cf83ead6f03c2821
Dmitry Korzhel <91656391+ferdinant122@users.noreply.github.com>
2022-06-20 22:48:22 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,30 @@ resource "digitalocean_kubernetes_cluster" "kubernetes_cluster" {
 output "cluster-id" {
   value = digitalocean_kubernetes_cluster.kubernetes_cluster.id
 }
+
+resource "digitalocean_droplet" "web" {
+  name   = "web-1"
+  size   = "s-1vcpu-1gb"
+  image  = "ubuntu-20-04-x64"
+  region = "fra1"
+}
+
+resource "digitalocean_loadbalancer" "public" {
+  name   = "loadbalancer-1"
+  region = "fra1"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+  }
+
+  droplet_ids = [digitalocean_droplet.web.id]
+}
