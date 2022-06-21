eedaf9b285d5eb932794a1308c8bb99960c386df
Dmitry Korzhel <91656391+ferdinant122@users.noreply.github.com>
2022-06-21 11:08:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.4.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
+      version = ">= 2.7.0"
     }
     helm = {
       source  = "hashicorp/helm"
