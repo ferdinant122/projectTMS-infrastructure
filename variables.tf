fc7519135ac0cc7a6d47f8666f73707651e5f434
Dmitry Korzhel <91656391+ferdinant122@users.noreply.github.com>
2022-06-21 21:43:45 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "cluster_name" {
   description = "The name of the kubernetes cluster to create"
   type        = string
-  default     = "dk-cluster"
+  default     = "dk-dev-cluster"
 }
 
 variable "cluster_version" {
@@ -19,7 +19,7 @@ variable "region" {
 variable "min_nodes" {
   description = "The minimum number of nodes in the default pool"
   type        = number
-  default     = 1
+  default     = 3
 }
 
 variable "max_nodes" {
@@ -31,5 +31,5 @@ variable "max_nodes" {
 variable "default_node_size" {
   description = "The default digital ocean node slug for each node in the default pool"
   type        = string
-  default     = "s-1vcpu-2gb"
+  default     = "s-2vcpu-4gb"
 }
