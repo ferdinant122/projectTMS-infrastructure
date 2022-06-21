07866f22ad3d4d95ce160577d75d9ee7f4fe0e1a
Dmitry Korzhel <91656391+ferdinant122@users.noreply.github.com>
2022-06-21 09:43:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,71 +5,27 @@ terraform {
       version = "~> 2.0"
     }
   }
-
+}
 
 provider "digitalocean" {
-  token = "dop_v1_22846706629dfdd1c3ed90bbe9e11b75115bdcc25755756161208354fd5794ba"
+  token = "dop_v1_4a2d38233587c1574a2eb9925af81b0d618a0ef50f150e1406ed87930e3a8f2c"
 }
 
-
-provider "kubernetes" {
-  host  = resource.digitalocean_kubernetes_cluster.k8s.endpoint
-  token = resource.digitalocean_kubernetes_cluster.k8s.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    resource.digitalocean_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
-  )
-}
-
-resource "digitalocean_kubernetes_cluster" "k8s" {
-  name   = "dk-cluster"
+resource "digitalocean_kubernetes_cluster" "dk-cluster" {
+  name   = "dk-cluste"
   region = "fra1"
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.22.8-do.1"
 
   node_pool {
-    name = "dk-cluster-worker-pool"
-    auto_scale = true
-    # doctl kubernetes options sizes
-    size       = "s-2vcpu-4gb"
-    min_nodes  = 1
-    max_nodes  = 5
+    name       = "dk-worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 3
+
+    taint {
+      key    = "workloadKind"
+      value  = "database"
+      effect = "NoSchedule"
+    }
   }
-}
-
-resource "digitalocean_loadbalancer" "public" {
-  name   = "loadbalancer-dk"
-  region = "fra1"
-
-
-  forwarding_rule {
-    entry_port     = 80
-    entry_protocol = "http"
-
-    target_port     = 80
-    target_protocol = "http"
-  }
-
-  healthcheck {
-    port     = 22
-    protocol = "tcp"
-  }
- }
-  
- resource "digitalocean_database_connection_pool" "pool-db-dk" {
-  cluster_id = digitalocean_database_cluster.postgres-example.id
-  name       = "pool-db-dk"
-  mode       = "transaction"
-  size       = 20
-  db_name    = "defaultdb"
-  user       = "doadmin"
- }
-
-resource "digitalocean_database_cluster" "postgres-dk" {
-  name       = "example-postgres-cluster"
-  engine     = "pg"
-  version    = "11"
-  size       = "db-s-1vcpu-1gb"
-  region     = "fra1"
-  node_count = 1
- }
 }
