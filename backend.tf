618f2194b8256f2e6a908369f695e0a99e50a5a4
Dmitry Korzhel <91656391+ferdinant122@users.noreply.github.com>
2022-06-21 11:28:24 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,8 @@ terraform {
     key                         = "terraform.tfstate"
     bucket                      = "terraform.state"
     region                      = "fra1"
+    access_key                  = "Z6FTY3CMC4ZPBGLI5F5Y"
+    secret_key                  = "bR8UKUKlIMPae8SOs9XyXuni1NAcxnllW0aQ8TOIqtY"
     skip_requesting_account_id  = true
     skip_credentials_validation = true
     skip_get_ec2_platforms      = true
