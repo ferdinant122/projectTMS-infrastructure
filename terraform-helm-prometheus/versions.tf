1ff3d7ffb84d4b1a1f00998dd9208b9c06ac9711
Dmitry Korzhel <dimakorp@gmail.com>
2022-06-23 09:46:53 +0300

    Update versions.tf

--- terraform-helm-prometheus/versions.tf
+++ terraform-helm-prometheus/versions.tf
@@ -19,21 +19,3 @@ terraform {
 provider "digitalocean" {
   token = var.do_token
 }
-
-provider "kubernetes" {
-  host  = digitalocean_kubernetes_cluster.monitoring-cluster.endpoint
-  token = digitalocean_kubernetes_cluster.monitoring-cluster.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    digitalocean_kubernetes_cluster.monitoring-cluster.kube_config[0].cluster_ca_certificate
-  )
-}
-
-provider "helm" {
-  kubernetes {
-    host  = digitalocean_kubernetes_cluster.monitoring-cluster.endpoint
-    token = digitalocean_kubernetes_cluster.monitoring-cluster.kube_config[0].token
-    cluster_ca_certificate = base64decode(
-      digitalocean_kubernetes_cluster.monitoring-cluster.kube_config[0].cluster_ca_certificate
-    )
-  }
-}
