2ad106139070e34340baf8da203ee857b18a58bc
Dmitry Korzhel <dimakorp@gmail.com>
2022-06-23 09:50:51 +0300

    delete prometheus

--- terraform-helm-prometheus/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "prometheus_query_api_url" {
-  description = "Prometheus query API URL: https://prometheus.io/docs/prometheus/latest/querying/api/#expression-queries"
-  value       = local.prometheus_query_api_url
-}
-
-output "prometheus_alerts_api_url" {
-  description = "Prometheus query API URL: https://prometheus.io/docs/prometheus/latest/querying/api/#expression-queries"
-  value       = local.prometheus_alerts_api_url
-}
