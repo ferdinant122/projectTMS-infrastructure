eb9a04da3623a5227912e1f5788de624495cdf09
Dmitry Korzhel <dimakorp@gmail.com>
2022-06-23 09:35:43 +0300

    Update versions.tf

--- terraform-helm-prometheus/versions.tf
+++ terraform-helm-prometheus/versions.tf
@@ -1,8 +1,39 @@
 terraform {
   required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.9.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.4.1"
     }
+  }
+}
+
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+provider "kubernetes" {
+  host  = digitalocean_kubernetes_cluster.dk-cluster.endpoint
+  token = digitalocean_kubernetes_cluster.dk-cluster.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.dk-cluster.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.dk-cluster.endpoint
+    token = digitalocean_kubernetes_cluster.dk-cluster.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.dk-cluster.kube_config[0].cluster_ca_certificate
+    )
   }
 }
